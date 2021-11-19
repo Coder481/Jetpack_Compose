0fb8229effef9fe2be9a92a114810bf017a01318
Coder481 <hrithiksharma24092000@gmail.com>
2021-11-19 21:44:10 +0530

    Columns and Raws

--- app/src/main/java/com/hrithik/composebasics/MainActivity.kt
+++ app/src/main/java/com/hrithik/composebasics/MainActivity.kt
@@ -3,166 +3,211 @@ package com.hrithik.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.hrithik.composebasics.ui.theme.ComposeBasicsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalCoilApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp {
-                MyGreetings()
+            ComposeBasicsTheme {
+                LazyList()
             }
         }
     }
 }
 
-@Composable
-fun MyApp(){
 
-    var isSelected by remember { mutableStateOf(false)}
 
-    ComposeBasicsTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = Color.Red) {
-            Greeting("Android",isSelected = isSelected, updateSelection = { newSelect -> isSelected = newSelect})
-        }
-    }
-}
+
 
 @Composable
-fun MyApp(content: @Composable () -> Unit) {
-    ComposeBasicsTheme {
-        Surface(color = Color.Yellow) {
-            content()
-        }
-    }
-}
-
-@Composable
-fun NameList(
-    names: List<String>,
-    modifier: Modifier = Modifier,
-    horizontalAlignment: Alignment.Horizontal
-) {
-
-    LazyColumn(
-        modifier = modifier
-        , horizontalAlignment = horizontalAlignment
-    ) {
-        items(
-            items = names
-        ) { name ->
-
-            var isSelected by remember { mutableStateOf(false)}
-
-            Greeting(
-                name = name,
-                isSelected = isSelected,
-                updateSelection = { newSelect ->
-                    isSelected = newSelect
+fun LayoutsCodelab() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
                 }
             )
+        }
+    ){ innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+    }
+}
 
-            Divider(color = Color.Black)
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+
+
+
+
+
+
+@Composable
+fun SimpleList() {
+    // We save the scrolling position with this state that can also
+    // be used to programmatically scroll the list
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
         }
     }
 }
 
+@ExperimentalCoilApi
 @Composable
-fun MyGreetings(names: List<String> = List(1000) { "Hello Android #$it" }){
+fun LazyList() {
+    // We save the scrolling position with this state that can also
+    // be used to programmatically scroll the list
+    val scrollState = rememberLazyListState()
 
-    val counterState = remember { mutableStateOf(0) }
+    val listSize = 100
 
-    Column (
-        modifier = Modifier.fillMaxHeight()
-        ,verticalArrangement = Arrangement.Center
-        , horizontalAlignment = Alignment.CenterHorizontally
-    ){
-//        Column(modifier = Modifier.weight(1f)
-//            , horizontalAlignment = Alignment.CenterHorizontally) {
-//            for (name : String in names){
-//                Greeting(name = name)
-//                Divider(color = Color.Black)
-//            }
-//        }
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
 
-        NameList(
-            names = names
-            , modifier = Modifier.weight(1f)
-            , horizontalAlignment = Alignment.CenterHorizontally
-        )
-
-//        Divider(color = Color.Transparent, thickness = 32.dp)
-        Counter(
-            count = counterState.value,
-            updateCount = { newCount ->
-                counterState.value = newCount
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
             }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
         )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+
+/**
+ * CompositionLocalProvider :   It allows us to pass data implicitly through the composition tree.
+ * LocalContentAlpha        :   Defining opacity level of its children
+ */
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .clickable { "" }
+            .clip(RoundedCornerShape(8.dp))
+            .shadow(3.dp)
+            .background(MaterialTheme.colors.surface)
+            .padding(16.dp)
+    ){
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    ComposeBasicsTheme {
+        PhotographerCard()
     }
 }
 
 @Composable
-fun Counter() {
-
-    val count = remember { mutableStateOf(0) }
-
-    Button(onClick = { count.value++ }, modifier = Modifier.padding(32.dp)) {
-        Text("I've been clicked ${count.value} times")
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
-@Composable
-fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    OutlinedButton(
-        onClick = { updateCount(count+1) },
-        modifier = Modifier.padding(32.dp),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = if (count > 5) Color.Green else Color.White
-        )
-    ) {
 
-        Text("I've been clicked $count times")
-
-    }
-}
-
-@Composable
-fun Greeting(name: String, isSelected: Boolean, updateSelection : (Boolean) -> Unit) {
-
-//    var isSelected by remember { mutableStateOf(false)}
-    val backgroundColor by animateColorAsState(if(isSelected) Color.Green else Color.Transparent)
-
-
-    Text(text = "Hello $name!"
-        , modifier = Modifier.padding(12.dp)
-                    .background(color = backgroundColor)
-                    .clickable (onClick = {updateSelection(!isSelected)})
-        ,color = Color.Magenta
-        ,style = MaterialTheme.typography.h4)
-}
 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApp {
-        MyGreetings()
+    ComposeBasicsTheme {
+        LayoutsCodelab()
     }
 }
